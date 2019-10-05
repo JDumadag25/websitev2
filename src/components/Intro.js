789f28457eaa927e639abfd4fb15afdb64d823bf
JDumadag25 <justin.dumadag@gmail.com>
2019-10-04 23:40:33 -0400

    playing with semantic ui to style

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Container, Segment, Icon, Button } from 'semantic-ui-react'
+import { Container, Segment, Icon, Button, Grid } from 'semantic-ui-react'
 
 import { Link } from 'react-router-dom'
 
@@ -8,25 +8,30 @@ class Intro extends React.Component {
 
   render(){
     return(
-      <div className='introduction'>
-      <Container textAlign='center'>
-        <h2>Who I am?</h2>
-          <p>I build interactive and beautiful web pages and software with a goal of making the world a little better off.</p>
-      </Container>
 
       <Container textAlign='center'>
+        <Grid columns={2} divided>
+          <Grid.Column>
+        <h2>Who I am?</h2>
+          <p>I build interactive and beautiful web pages and software with a goal of making the world a little better off.</p>
+          </Grid.Column>
+          <Grid.Column>
         <h3>Skills</h3>
-        <Segment.Group horizontal>
+
+      <Segment.Group horizontal>
         <Segment> <Icon name='code'/> </Segment>
         <Segment> <Icon name='code'/> </Segment>
         <Segment> <Icon name='code'/> </Segment>
       </Segment.Group>
+  </Grid.Column>
 
       <Link to='/projects'>
         <Button>Check out my projects</Button>
       </Link>
+
+      </Grid>
     </Container>
-</div>
+
     )
   }
 }
